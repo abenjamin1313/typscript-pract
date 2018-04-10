e72abf4ab2742b75087eeb168bf66feee755ba31
Adam Victor <abenjamin1313@gmail.com>
2018-04-10 10:43:57 -0400

    Updated types file.

--- type.js
+++ type.js
@@ -1,21 +1,32 @@
+// standard types
 var myString;
 var myNum;
 var myBool;
 var myVar;
+// ARRAYS
 var strArr;
 var numArr;
 var boolArr;
+// tuple array
+var strNumTuple;
+// standard type
 myString = 'Hello'.slice(0, 3);
 myNum = 1;
 myBool = false;
 myVar = true;
+// ARRAYS
 strArr = ['Hello', 'World'];
 numArr = [0, 1, 1, 2, 3, 5, 8, 13, 21];
 boolArr = [true, false, false, true, true, false];
+// Tuple
+strNumTuple = [1, 'John', 'Doe'];
+// output
+console.log('START of TYPES');
 console.log(myString);
 console.log(myNum);
 console.log(myBool);
 console.log(myVar);
 console.log(strArr);
 console.log(numArr);
-console.log(boolArr);
+console.log(strNumTuple);
+console.log('END of TYPES');
--- type.ts
+++ type.ts
@@ -1,25 +1,35 @@
+// standard types
 let myString: string;
 let myNum: number;
 let myBool: boolean;
 let myVar: any;
-
+// ARRAYS
 let strArr: string[];
 let numArr: number[];
 let boolArr: boolean[];
+// tuple array
+let strNumTuple: [number, string, string];
 
+// standard type
 myString = 'Hello'.slice(0, 3);
 myNum = 1;
 myBool = false;
 myVar = true;
 
+// ARRAYS
 strArr = ['Hello', 'World'];
 numArr = [0, 1, 1, 2, 3, 5, 8, 13, 21]
 boolArr = [true, false, false, true, true, false]
+// Tuple
+strNumTuple = [1, 'John', 'Doe'];
 
+// output
+console.log('START of TYPES');
 console.log(myString);
 console.log(myNum);
 console.log(myBool);
 console.log(myVar);
 console.log(strArr);
 console.log(numArr);
-console.log(boolArr);
+console.log(strNumTuple);
+console.log('END of TYPES');
