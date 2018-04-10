737ac21fab6244cf02de5c36e876366a6a2506ab
Adam Victor <abenjamin1313@gmail.com>
2018-04-10 10:31:05 -0400

    Intial Commit.

---
+++ type.ts
@@ -0,0 +1,25 @@
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+
+myString = 'Hello'.slice(0, 3);
+myNum = 1;
+myBool = false;
+myVar = true;
+
+strArr = ['Hello', 'World'];
+numArr = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+boolArr = [true, false, false, true, true, false]
+
+console.log(myString);
+console.log(myNum);
+console.log(myBool);
+console.log(myVar);
+console.log(strArr);
+console.log(numArr);
+console.log(boolArr);
